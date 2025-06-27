143d2f4ffa7699f4b793a13a48071b943b0f20f2
saif64s <my@wo.rk>
2025-06-27 20:15:19 +0600

    Add basic  model for user

--- src/main/java/com/pixel/mealz/mealz/features/auth/model/User.java
+++ src/main/java/com/pixel/mealz/mealz/features/auth/model/User.java
@@ -1,4 +1,64 @@
 package com.pixel.mealz.mealz.features.auth.model;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "app_users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column()
+    private String employeeName;
+
+    @Column(nullable = false)
+    private String officeLocation;
+
+    @Column()
+    private String phoneNumber;
+
+    @Column()
+    private Date createdAt;
+
+    @Column()
+    private Date updatedAt;
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
 }
